8ed7cc80f3000e1beeac6d69d0c015941e08dcc9
Snyp <gabrielmarques.mg@gmail.com>
2020-04-09 14:42:28 -0300

    [Edit]_Tests

--- src/test/java/apitest/APITest.java
+++ src/test/java/apitest/APITest.java
@@ -8,59 +8,27 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 public class APITest {
-	
+
 	@BeforeClass
 	public static void setup() {
 		RestAssured.baseURI = "http://localhost:8001/tasks-backend";
 	}
 
 	@Test
-	public void deveRetornarTarefas() {
-		RestAssured.given()
-		.when()
-			.get("/todo")
-		.then()
-			.statusCode(200);
+	public void returnTasks() {
+		RestAssured.given().when().get("/todo").then().statusCode(200);
 	}
-	
+
 	@Test
-	public void deveAdicionarTarefaComSucesso() {
-		RestAssured.given()
-			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2020-12-30\" }")
-			.contentType(ContentType.JSON)
-		.when()
-			.post("/todo")
-		.then()
-			.statusCode(201);
+	public void addTaskWithSuccess() {
+		RestAssured.given().body("{ \"task\": \"Teste via API\", \"dueDate\": \"2020-12-30\" }")
+				.contentType(ContentType.JSON).when().post("/todo").then().statusCode(201);
 	}
-	
+
 	@Test
-	public void naoDeveAdicionarTarefaInvalida() {
-		RestAssured.given()
-			.body("{ \"task\": \"Teste via API\", \"dueDate\": \"2010-12-30\" }")
-			.contentType(ContentType.JSON)
-		.when()
-			.post("/todo")
-		.then()
-			.statusCode(400)
-			.body("message", CoreMatchers.is("Due date must not be in past"));
-	}
-	
-	@Test
-	public void deveRemoverTarefaComSucesso() {
-		Integer id = RestAssured.given()
-			.body("{ \"task\": \"Tarefa para remoção\", \"dueDate\": \"2020-12-30\" }")
-			.contentType(ContentType.JSON)
-		.when()
-			.post("/todo")
-		.then()
-			.statusCode(201)
-			.extract().path("id");
-		
-		RestAssured.given()
-		.when()
-			.delete("/todo/"+id)
-		.then()
-			.statusCode(204);
+	public void dontAddTasksWithFailure() {
+		RestAssured.given().body("{ \"task\": \"Teste via API\", \"dueDate\": \"2010-12-30\" }")
+				.contentType(ContentType.JSON).when().post("/todo").then().statusCode(400)
+				.body("message", CoreMatchers.is("Due date must not be in past"));
 	}
 }
